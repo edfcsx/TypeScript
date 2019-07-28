9d0b78eb77ae5302f70948f386eed0430e62dfe5
eduardo cipriano <edfcsx@gmail.com>
2019-07-28 13:28:52 -0300

    was added abstract, singleton class in class module

--- class/examples/product.class.ts
+++ class/examples/product.class.ts
@@ -5,13 +5,22 @@ class Product {
     public discount: number = 0, 
   ) {}
 
-  public resume = () => {
-    console.log(`Product ${this.name}, price is ${this.price - this.discount}`)
+  public resume = ():string => {
+    if (this.discount > 0) {
+      const price = this.priceWithDescount();
+      return `Product: ${this.name}, price is R$ ${this.price}, have a (${this.discount * 100}%) off, final price is ${price}`
+    } else {
+      return `Product: ${this.name}, price is R$ ${this.price}`;
+    }
+  }
+
+  private priceWithDescount = ():number => {
+    return this.price - (this.price * this.discount);
   }
 }
 
 const productX = new Product('Arroz', 5);
-const productY = new Product('Salsicha', 10, 2);
+const productY = new Product('Salsicha', 10, 0.2);
 
-productX.resume();
-productY.resume();
+console.log(productX.resume());
+console.log(productY.resume());

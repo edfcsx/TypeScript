81821198b7bada73ac965a0795497f2f7b097fff
edfcsx <edfcsx@gmail.com>
2019-07-29 14:26:00 -0300

    final files for modules

--- namespaces/namespaces.js
+++ namespaces/namespaces.js
@@ -1,24 +1,4 @@
-var Geometry;
-(function (Geometry) {
-    var Area;
-    (function (Area) {
-        function rectangle(base, height) {
-            return base * height;
-        }
-        Area.rectangle = rectangle;
-    })(Area = Geometry.Area || (Geometry.Area = {}));
-})(Geometry || (Geometry = {}));
-var Geometry;
-(function (Geometry) {
-    var Area;
-    (function (Area) {
-        var PI = 3.14;
-        function circumference(radius) {
-            return PI * Math.pow(radius, 2);
-        }
-        Area.circumference = circumference;
-    })(Area = Geometry.Area || (Geometry.Area = {}));
-})(Geometry || (Geometry = {}));
+"use strict";
 ///<reference path="./geometryRectangle.ts" />
 ///<reference path="./geometryCircle.ts" />
 // Line commander for compiler file with references
